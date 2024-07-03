4a96f8d03a662e70aac761dd04fd606e836d004b
araya_a <arayaanderson@gmail.com>
2024-07-03 15:53:35 -0500

    Fetch recipes and create recipe component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./components/Header";
+import RecipeExcerpt from "./components/RecipeExcerpt";
+import RecipeFull from "./components/RecipeFull";
 import "./App.css";
 
 function App() {
+  const [recipes, setRecipes] = useState ([])
+  const [selectedRecipe, setSelectedRecipe] = useState(null)
+
+  useEffect(() => {
+    const fetchAllRecipes = async () => {
+      try {
+        const response = await fetch("/api/recipes")
+        if (response.ok){
+          const data = await response.json()
+          setRecipes(data)
+        } else {
+          console.log("Opps - could not fetch recipes!");
+        }
+      } catch(e) {
+        console.error("An error occured during the request" , e)
+
+      }
+    };
+    fetchAllRecipes()
+  }, []);
+
+  const handleSelectRecipe = (recipe) => {
+    setSelectedRecipe(recipe);
+  };
+
+  const handleUnselectRecipe = () => {
+    setSelectedRecipe(null);
+  };
+
   return (
     <div className='recipe-app'>
       <Header />
-      <p>Your recipes here! </p>
+      {selectedRecipe && <RecipeFull selectedRecipe={selectedRecipe} handleUnselectRecipe={handleUnselectRecipe} />}
+      {!selectedRecipe && (
+        <div className='recipe-list'>
+          {recipes.map((recipe) => (
+            <RecipeExcerpt key={recipe.id} recipe={recipe} handleSelectRecipe={handleSelectRecipe} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/components/RecipeExcerpt.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const RecipeExcerpt = ({recipe, handleSelectRecipe}) => {
+    return (
+        <article className="recipe-card">
+            <figure>
+                <img src={recipe.image_url} alt={recipe.title} />
+
+            </figure>
+            <h2>{recipe.title}</h2>
+            <p className="flex-spacing">Decription: {recipe.description}</p>
+            <button onClick={()=> handleSelectRecipe(recipe)}>View</button>
+
+        </article>
+        
+    )
+}
+
+export default RecipeExcerpt
